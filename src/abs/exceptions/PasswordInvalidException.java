b63c8c7721e006c5ca40d2d10274a0dc7bbc57f1
Daniel <s3383022@student.rmit.edu.au>
2017-03-28 03:21:31 +1100

    Updated password exception

---
+++ src/abs/exceptions/PasswordInvalidException.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package abs.exceptions;
+
+/**
+ * The password invalid exception is thrown when a user tried to login with a
+ * valid username but invalid password.
+ *
+ */
+public class PasswordInvalidException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4L;
+
+	public PasswordInvalidException(String message) {
+		super("Im sorry but the password you entered is invalid. Please try again.");
+	}
+
+}
