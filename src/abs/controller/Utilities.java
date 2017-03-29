f9d2a026c13c195886d81341ff14c4ecf5254057
Daniel <s3383022@student.rmit.edu.au>
2017-03-30 06:33:57 +1100

    updated ready for part B
    this updates the tests, dummy data and utils class to support mutipal businesses.

--- src/abs/controller/Utilities.java
+++ src/abs/controller/Utilities.java
@@ -100,96 +100,99 @@ public class Utilities {
 	public int readBusinessData() {
 
 		// TODO update method to loop for multiple businesses
+		businesses = new ArrayList<Business>();
 
 		try {
 			FileReader reader = new FileReader(filePath + businessInfoFileName);
 			BufferedReader bufferedReader = new BufferedReader(reader);
 
-			if (bufferedReader.readLine().contains("Business Info")) {
-				String name; // Business name
-				String desc; // Business description
-				String address; // Business address
-				int number; // Business contact number
+			String firstLine;
+			while ((firstLine = bufferedReader.readLine()) != null) {
+				if (firstLine.contains("Business Info")) {
+					String name; // Business name
+					String desc; // Business description
+					String address; // Business address
+					int number; // Business contact number
 
-				// Business's employees
-				List<Employee> staff = new ArrayList<Employee>();
+					// Business's employees
+					List<Employee> staff = new ArrayList<Employee>();
 
-				// Available bookings
-				List<Booking> avBookings = new ArrayList<Booking>();
+					// Available bookings
+					List<Booking> avBookings = new ArrayList<Booking>();
 
-				businesses = new ArrayList<Business>();
+					name = bufferedReader.readLine();
+					desc = bufferedReader.readLine();
+					address = bufferedReader.readLine();
+					number = Integer.parseInt(bufferedReader.readLine());
 
-				name = bufferedReader.readLine();
-				desc = bufferedReader.readLine();
-				address = bufferedReader.readLine();
-				number = Integer.parseInt(bufferedReader.readLine());
+					// Create owner user.
+					String ownerName = bufferedReader.readLine();
+					String ownerEmail = bufferedReader.readLine();
+					String ownerPass = bufferedReader.readLine();// TODO update
+																	// for
+																	// encryption
 
-				// Create owner user.
-				String ownerName = bufferedReader.readLine();
-				String ownerEmail = bufferedReader.readLine();
-				String ownerPass = bufferedReader.readLine();// TODO update for
-																// encryption
+					Owner owner = new Owner(ownerName, ownerEmail, ownerPass);
 
-				Owner owner = new Owner(ownerName, ownerEmail, ownerPass);
+					// Checks if document is empty
+					String emTest = bufferedReader.readLine();
+					if ((emTest != null)) {
 
-				// Checks if document is empty
-				String emTest = bufferedReader.readLine();
-				if ((emTest != null)) {
+						// Checks for employee data
+						if (emTest.contains("Employees")) {
+							String[] employee;
+							// Employee data is CSV
+							while ((employee = bufferedReader.readLine().split(splitChar)).length > 1) {
 
-					// Checks for employee data
-					if (emTest.contains("Employees")) {
-						String[] employee;
-						// Employee data is CSV
-						while ((employee = bufferedReader.readLine().split(splitChar)).length > 1) {
+								List<Availability> availabilities = null;
+								availabilities = new ArrayList<Availability>();
 
-							List<Availability> availabilities = null;
-							availabilities = new ArrayList<Availability>();
+								// Adds the employees availabilities, if any.
+								for (int i = 1; i < employee.length; i++) {
+									i++;
+									availabilities.add(new Availability(employee[i - 1], employee[i]));
+								} // close for
 
-							// Adds the employees availabilities, if any.
-							for (int i = 1; i < employee.length; i++) {
-								i++;
-								availabilities.add(new Availability(employee[i - 1], employee[i]));
-							} // close for
+								// adds new employee
+								staff.add(new Employee(employee[0], availabilities));
+							} // close while
 
-							// adds new employee
-							staff.add(new Employee(employee[0], availabilities));
-						} // close while
+						} // else format incorrect
 
-					} // else format incorrect
+						String line = bufferedReader.readLine();
+						if (line != null) {
+							String[] bookings = line.split(splitChar);
 
-					String line = bufferedReader.readLine();
-					if (line != null) {
-						String[] bookings = line.split(splitChar);
+							if (bookings.length > 1) {
 
-						if (bookings.length > 1) {
+								for (int i = 0; i < bookings.length; i++) { // for
+																			// all
+																			// bookings
+									i++;
+									avBookings.add(new Booking((new Availability(bookings[i], bookings[i + 1])),
+											bookings[i - 1]));
+									i++;
+									// TODO match employeeID to employee object.
+								} // close for
 
-							for (int i = 0; i < bookings.length; i++) { // for
-																		// all
-																		// bookings
-								i++;
-								avBookings.add(
-										new Booking((new Availability(bookings[i], bookings[i + 1])), bookings[i - 1]));
-								i++;
-								// TODO match employeeID to employee object.
-							} // close for
+							} // Else incorrect bookings format
+						} // Else no bookings
+					} // Else no employees or bookings
 
-						} // Else incorrect bookings format
-					} // Else no bookings
-				} // Else no employees or bookings
+					businesses.add(new Business(name, desc, address, number, staff, avBookings, owner));
 
-				businesses.add(new Business(name, desc, address, number, staff, avBookings, owner));
+				} else {
 
-				reader.close(); // Close file
-				return 0; // Success
-			} else {
-				reader.close(); // Close file
-				return -2; // File exists but is empty or not formatted
-							// correctly
+					return -2; // File exists but is empty or not formatted
+								// correctly
+				}
 			}
 
-		} catch (
+			bufferedReader.close(); // Close file
+			reader.close(); // Close file
+			return 0; // Success
 
-		FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			return -1; // File not found
 
 		} catch (IOException e) {
@@ -270,57 +273,59 @@ public class Utilities {
 
 			// TODO i = 0 for now, when multi business surround with loop
 
-			int k = 0;
+			for (Business business : businessesWr) {
 
-			// Wrote business info
-			bufferedWriter.write("# Business Info\n");
-			bufferedWriter.write(businessesWr.get(k).getName());
-			bufferedWriter.newLine();
-			bufferedWriter.write(businessesWr.get(k).getDesc());
-			bufferedWriter.newLine();
-			bufferedWriter.write(businessesWr.get(k).getAddress());
-			bufferedWriter.newLine();
-			bufferedWriter.write(Integer.toString(businessesWr.get(k).getPhone()));
-			bufferedWriter.newLine();
-
-			// write owner user info
-			Owner owner = businessesWr.get(k).getOwner();
-			bufferedWriter.write(owner.getName());
-			bufferedWriter.newLine();
-			bufferedWriter.write(owner.getEmail());
-			bufferedWriter.newLine();
-			bufferedWriter.write(owner.getPassword());
-			bufferedWriter.newLine();
-
-			// Write employee data
-			bufferedWriter.write("# Employees\n");
-			List<Employee> staff = businessesWr.get(k).getStaff();
-			for (int i = 0; i < staff.size(); i++) {
-				bufferedWriter.write(staff.get(i).getName());
-				List<Availability> staffAvalib = staff.get(i).getAvailabilities();
-				for (int j = 0; j < staffAvalib.size(); j++) {
-					bufferedWriter.write(splitChar + staffAvalib.get(j).getDate());
-					bufferedWriter.write(splitChar + staffAvalib.get(j).getTime());
-				}
+				// Wrote business info
+				bufferedWriter.write("# Business Info\n");
+				bufferedWriter.write(business.getName());
 				bufferedWriter.newLine();
-			}
+				bufferedWriter.write(business.getDesc());
+				bufferedWriter.newLine();
+				bufferedWriter.write(business.getAddress());
+				bufferedWriter.newLine();
+				bufferedWriter.write(Integer.toString(business.getPhone()));
+				bufferedWriter.newLine();
 
-			// Write available bookings data
-			bufferedWriter.write("# Bookings\n");
-			List<Booking> avBookings = businessesWr.get(k).getAvBookings();
-			for (int i = 0; i < avBookings.size(); i++) {
-				if (i == 0) {
-					bufferedWriter.write(avBookings.get(i).getStaff());
-					Availability slot = avBookings.get(i).getSlot();
-					bufferedWriter.write(splitChar + slot.getDate());
-					bufferedWriter.write(splitChar + slot.getTime());
-				} else {
-					bufferedWriter.write(splitChar + avBookings.get(i).getStaff());
-					Availability slot = avBookings.get(i).getSlot();
-					bufferedWriter.write(splitChar + slot.getDate());
-					bufferedWriter.write(splitChar + slot.getTime());
+				// write owner user info
+				Owner owner = business.getOwner();
+				bufferedWriter.write(owner.getName());
+				bufferedWriter.newLine();
+				bufferedWriter.write(owner.getEmail());
+				bufferedWriter.newLine();
+				bufferedWriter.write(owner.getPassword());
+				bufferedWriter.newLine();
+
+				// Write employee data
+				bufferedWriter.write("# Employees\n");
+				List<Employee> staff = business.getStaff();
+				for (int i = 0; i < staff.size(); i++) {
+					bufferedWriter.write(staff.get(i).getName());
+					List<Availability> staffAvalib = staff.get(i).getAvailabilities();
+					for (int j = 0; j < staffAvalib.size(); j++) {
+						bufferedWriter.write(splitChar + staffAvalib.get(j).getDate());
+						bufferedWriter.write(splitChar + staffAvalib.get(j).getTime());
+					}
+					bufferedWriter.newLine();
 				}
 
+				// Write available bookings data
+				bufferedWriter.write("# Bookings\n");
+				List<Booking> avBookings = business.getAvBookings();
+				for (int i = 0; i < avBookings.size(); i++) {
+					if (i == 0) {
+						bufferedWriter.write(avBookings.get(i).getStaff());
+						Availability slot = avBookings.get(i).getSlot();
+						bufferedWriter.write(splitChar + slot.getDate());
+						bufferedWriter.write(splitChar + slot.getTime());
+					} else {
+						bufferedWriter.write(splitChar + avBookings.get(i).getStaff());
+						Availability slot = avBookings.get(i).getSlot();
+						bufferedWriter.write(splitChar + slot.getDate());
+						bufferedWriter.write(splitChar + slot.getTime());
+					}
+
+				}
+				bufferedWriter.write("\n");
 			}
 
 			return 0;
@@ -397,7 +402,7 @@ public class Utilities {
 	 */
 	public int writeData(List<Business> businessesWr, List<User> customerWr) { // TODO
 
-		int bus = writeBusinessData(businesses);
+		int bus = writeBusinessData(businessesWr);
 		int cus = writeCustomerData(customerWr);
 		if ((bus == 0) && (cus == 0)) {
 			return 0;
