7ac0f1dbbf2293e259b204d4d53df9b2ae747897
RedBeardCory <corypatto@gmail.com>
2017-03-30 19:03:35 +1100

    added business to owner class added booking list and appropriate setters and getters to the list for abstract user class

--- src/abs/model/Owner.java
+++ src/abs/model/Owner.java
@@ -2,10 +2,25 @@ package abs.model;
 // Owner Class (implements User)
 
 public class Owner extends AbstractUser {
+	
+	private Business business;
 
 	public Owner(String name, String email, String password) {
 		super(name, email, password);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		return "Owner [name=" + this.getName() + ", email=" + this.getEmail() + ", password=" + this.getPassword() + "]";
+	}
+
+	public Business getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(Business business) {
+		this.business = business;
 	}
 
 }
