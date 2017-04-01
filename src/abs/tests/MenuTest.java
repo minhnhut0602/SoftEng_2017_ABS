cd7f9e9970ac00b44b30e64ee3ccd039b47328e1
Daniel <s3383022@student.rmit.edu.au>
2017-04-01 15:24:51 +1100

    Added another menu test
    Test for displaying the customer dash

--- src/abs/tests/MenuTest.java
+++ src/abs/tests/MenuTest.java
@@ -14,6 +14,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import abs.controller.UserAuth;
 import abs.controller.Utilities;
 import abs.model.Business;
 import abs.model.Customer;
@@ -135,9 +136,31 @@ public class MenuTest {
 
 	@Test
 	public void testMenuDispayCustomerDashboard() {
+		dummyIn = "0"; // Sending dummy input to menu
+		System.setIn(new ByteArrayInputStream(dummyIn.getBytes()));
 
-		fail("Not yet implemented"); // TODO
+		int mainMenu = menu.customerDashboard(); // Prints main menu,
+													// prompts for login
+		// or register.
+		String customerLoginEm = "stacy.d@gmail.com";
 
+		String customerLoginPa = "stacypassword";
+		UserAuth ua = new UserAuth();
+		boolean userLogin = ua.authUser(customerLoginEm, customerLoginPa);
+
+		if (!userLogin) {
+			fail("User failed to login");
+		}
+
+		String custName = "Stacy";
+
+		// TEST what method prints
+		String expectedOutput = "Welcome " + custName + " to the Appointment Booking System\n"
+				+ "Please select an option:\n" + "1. View avaliable bookings\n" + "2. View my bookings\n"
+				+ "3. Logout\n" + "4. Exit\n";
+
+		assertEquals(expectedOutput, outContent.toString()); // Compared the
+																// output of
 	}
 
 	@After
