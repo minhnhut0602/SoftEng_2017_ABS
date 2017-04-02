a60506e0f79bf3e1a6b03726bc7b87158aec25ea
RedBeardCory <corypatto@gmail.com>
2017-04-02 15:46:16 +1000

    started main class
    it has the main method. awaiting more info from other classes to be
    complete before doing more

---
+++ src/abs/view/Main.java
@@ -0,0 +1,28 @@
+package abs.view;
+
+public class Main {
+	
+	static Menu menu = new Menu();
+	
+	public static void main(String[] args) {
+		
+		boolean exit = false;
+		
+		while(exit == false){
+			//runs the main menu until exit == true
+			//each sub-menu is called by the switch statements inside each static method in this class
+			mainMenu();
+		}
+	}
+	
+	private static String mainMenu() {
+		menu.printMenu();
+		return menu.getInput();
+		
+		//switch statement for the main menu selections
+		
+	}
+	
+	//i'll need a new method for each of the main menu sub menus
+
+}
