2a486a08a345bbd03d2b923f199800d8e69d5b3e
Daniel <s3383022@student.rmit.edu.au>
2017-04-04 00:32:44 +1000

    added test suite
    a way of doing all tests at once

---
+++ src/abs/tests/AllTests.java
@@ -0,0 +1,17 @@
+package abs.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * This is a test suite to run all the test cases.
+ * 
+ * Note please add here any additional tests created.
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ FileTest.class, LoginTest.class, MenuTest.class })
+public class AllTests {
+
+}
