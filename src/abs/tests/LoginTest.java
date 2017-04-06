fcb3b071a476fd1596ccfb1e28604e7b8c17e96e
Daniel <s3383022@student.rmit.edu.au>
2017-04-06 23:01:30 +1000

    updated loginTest to handel the exceptions
    tests are now more accurate!

--- src/abs/tests/LoginTest.java
+++ src/abs/tests/LoginTest.java
@@ -5,11 +5,15 @@ package abs.tests;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import abs.controller.UserAuth;
+import abs.exceptions.PasswordInvalidException;
+import abs.exceptions.RegistrationNonUniqueException;
+import abs.exceptions.RegistrationValidationException;
 
 /**
  * 
@@ -33,14 +37,31 @@ public class LoginTest {
 	public void alreadyRegisteredUser() {
 		// try registering a use who is already registered
 
-		auth.registerUser(name, email, address, phone, pass);
-		assertFalse(auth.registerUser(name, email, address, phone, pass));
+		try {
+			auth.registerUser(name, email, address, phone, pass);
+		} catch (RegistrationNonUniqueException e) {
+			fail("Existing Error");
+		} catch (RegistrationValidationException e) {
+			fail("Format error");
+		}
+
+		try {
+			auth.registerUser(name, email, address, phone, pass);
+		} catch (RegistrationNonUniqueException e) {
+			assertTrue(true);
+		} catch (RegistrationValidationException e) {
+			fail("Format error");
+		}
 	}
 
 	@Test
 	public void loginWithoutRegistering() {
 		// use an invalid username and password
-		assertFalse(auth.authUser("unregistered@email.com", "unregisteredPassword12!"));
+		try {
+			auth.authUser("unregistered@email.com", "unregisteredPassword12!");
+		} catch (PasswordInvalidException e) {
+			assertTrue(true);
+		}
 	}
 
 	@Test
@@ -49,21 +70,44 @@ public class LoginTest {
 
 		// not sure if the testRegister is persistent so i registered the user
 		// again. but that might fail this one
-		auth.registerUser(name, email, address, phone, pass);
-		assertTrue(auth.authUser(email, pass));
+		try {
+			auth.registerUser(name, email, address, phone, pass);
+		} catch (RegistrationNonUniqueException e) {
+			fail("Existing Error");
+		} catch (RegistrationValidationException e) {
+			fail("Format error");
+		}
+
+		try {
+			assertTrue(auth.authUser(email, pass));
+		} catch (PasswordInvalidException e) {
+			fail("Credentials error");
+		}
 	}
 
 	@Test
 	public void noPasswordRegister() {
 		// try to register without a password
-		assertFalse(auth.registerUser(name, email, address, phone, ""));
+		try {
+			auth.registerUser(name, email, address, phone, "");
+		} catch (RegistrationNonUniqueException e) {
+			fail("Existing Error");
+		} catch (RegistrationValidationException e) {
+			assertTrue(true);
+		}
 	}
 
 	@Test
 	public void noUsernameRegister() {
 		// try registering without username
 
-		assertFalse(auth.registerUser("", email, address, phone, pass));
+		try {
+			assertFalse(auth.registerUser("", email, address, phone, pass));
+		} catch (RegistrationNonUniqueException e) {
+			fail("Existing Error");
+		} catch (RegistrationValidationException e) {
+			assertTrue(true);
+		}
 	}
 
 	/**
@@ -84,7 +128,13 @@ public class LoginTest {
 	 */
 	@Test
 	public void testLogin() {
-		boolean result = auth.authUser(email, pass);
+		boolean result = false;
+		try {
+			result = auth.authUser(email, pass);
+		} catch (PasswordInvalidException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertTrue(result == true);
 	}
 
@@ -95,7 +145,13 @@ public class LoginTest {
 	@Test
 	public void testRegister() {
 
-		boolean result = auth.registerUser(name, email, address, phone, pass);
+		boolean result = false;
+		try {
+			result = auth.registerUser(name, email, address, phone, pass);
+		} catch (RegistrationNonUniqueException | RegistrationValidationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertTrue(result == true);
 	}
 
