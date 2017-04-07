c308ec5dd8b100b29adfea2b542fd0032fa1d79d
rmit-s3606584-marco-riotto <s3606584@student.rmit.edu.au>
2017-04-08 02:45:18 +1000

    authentication redone
    some updates to authenticate

--- src/abs/controller/UserAuth.java
+++ src/abs/controller/UserAuth.java
@@ -16,17 +16,14 @@ public class UserAuth {
 	
 	public boolean authUser(String email, String Password) {
 		
-		/* Import customer list */
-		//this.customers = getCustomers();
 		
-
-		
-		/* Validate Email*/
+		/* VALIDATE EMAIL */
 		int counter, emailLength;
 		
-		/* checks if either the email or password was left empty, exit if empty */
 		if (email.length() == 0 || Password.length() == 0 || email.matches(".*[ \\(),:;<>[]\"].*")){
 			return false;
+			/* ERROR: password or email is empty */
+
 		}else{
 			
 			emailLength = email.length();
@@ -36,26 +33,29 @@ public class UserAuth {
 			/* loop through individual characters in the string 'email' and compares to '@' and '.com', there should only be one of each example in the string of each, and they must be in order '@' followed by '.'*/
 			for(counter = 0; counter <= emailLength-1; counter++){
 				
-				/* if there are more than one '@' or '.', exit fail */
 				if(counterAt > 1 || counterPeriod > 1){
 					return false;
+					/* ERROR: too many '@' or '.com' */
+
 				}else{
 					
-					/* if there is a @ found, +1 to counterAt */
 					if(email.charAt(counter) == '@'){
 						counterAt += 1;
 					}
-					/* if there doesnt exist an '@', but a '.com' has been found, exit fail */
+					
 					if(email.toLowerCase().contains(".com") && counterAt < 1){
 						return false;
+						/* ERROR: '.com' found before '@' */
+
 						
-					/* check again if there is an '.com', +1 to counterPeriod */
 					}else if(email.toLowerCase().contains(".com")){
 						counterPeriod += 1;
 
 					}
 					if(counterPeriod > 1){
 						return false;
+						/* ERROR: more than one '.com' found  */
+
 					}
 				}
 				
@@ -72,6 +72,8 @@ public class UserAuth {
 		/* ***NEEDS TO BE LOOKED AT*** this will check with current email accounts to ensure there are no already existing emails accounts by the same name */
 		if(email.equals(customers.get(0).getEmail())){
 			return false;
+			/* ERROR: email already exists */
+
 		}
 		
 
@@ -79,17 +81,21 @@ public class UserAuth {
 
 	public boolean registerUser(String name, String email, String password, String address, String phone) {
 		
-		/* Validate Name */
+		/* VALIDATE NAME */
 		if(name == null){
 			return false;
+			/* ERROR: name is empty */
+
 		}
 		
-		/* Validate Email & Password */
+		/* VVALIDATE EMAIL AND PASSWORD */
 		if(authUser(email, password) == false){
 			return false;
+			/* ERROR: email or password did not validate */
+
 		}
 		
-		/* Validate Address */
+		/* VALIDATE ADDRESS */
 		ArrayList<String> addressToks = new ArrayList<String>();
 		StringTokenizer addressTokeniser = new StringTokenizer(address);
 		int index = 0;
@@ -102,7 +108,54 @@ public class UserAuth {
 		
 		
 		
-		/* Validate Phone */
+		int currentFocus = 0;
+		if(addressToks.get(currentFocus).contains("[0-9]+")){
+			currentFocus++;
+		}
+		
+		while(currentFocus < addressToks.size()-3){
+			if(addressToks.get(currentFocus).contains("[a-zA-Z,]+")){
+				currentFocus++;
+			}else{
+				return false;
+				/* ERROR: line in address is not in correct format */
+			}
+
+		}
+		while(currentFocus < addressToks.size()-1){
+			
+		
+		if(addressToks.get(currentFocus).matches("[0-9]+")){
+			if(addressToks.get(currentFocus).length() == 4){
+				currentFocus++;
+			}else{
+				return false;
+				/* ERROR: Post code not 4 digits */
+
+			}
+		}
+		if(addressToks.get(currentFocus).matches("[a-zA-Z]+")){
+			if(addressToks.get(currentFocus).toUpperCase().matches("VIC|NSW|ACT|WA|SA|TAS")){
+				currentFocus++;
+			}else{
+				return false;
+				/* ERROR: invalid state provided */
+				
+			}
+		}
+		
+		}/* While loop end */
+		
+		
+		/* VALIDATE PHONE */
+		if(phone.replaceAll("\\s+","").matches("[0-9]+") && (phone.length() = 10 || phone.length() = 8)){
+			/* Do nothing */
+
+		}else{
+			return false;
+			/* ERROR: invalid state provided */
+
+		}
 		
 		
 		
