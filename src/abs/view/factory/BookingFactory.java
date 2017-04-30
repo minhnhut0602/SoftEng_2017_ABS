a87fc3071ba3e61d8ad947e44cb0a768f1019dd2
Daniel <s3383022@student.rmit.edu.au>
2017-04-30 23:44:34 +1000

    Updated bookings factory

--- src/abs/view/factory/BookingFactory.java
+++ src/abs/view/factory/BookingFactory.java
@@ -1,21 +1,20 @@
 package abs.view.factory;
 
-import javax.swing.JButton;
+import java.awt.Color;
+
 import javax.swing.JTextPane;
 
 import abs.model.Booking;
+import abs.view.GUIComponents.AvButton;
 
 public class BookingFactory {
 
 	public static String bookingString(Booking booking) {
 
-		// String length = (booking.getSlot().getBlocks()) / 2.0 + "hrs"; //
-		// Only works for bloacks of 30 min
-
 		String length = (booking.getSlot().getBlocks() * booking.getBusiness().BLOCK_LENGTH) + "Mins";
 
-		String bookingString = "Type: " + booking.getService() + "Date: " + booking.getSlot().getDate() + "Start Time: "
-				+ booking.getSlot().getTime() + "Length: " + length + "Staff: " + booking.getStaff().getName();
+		String bookingString = "Type: " + booking.getService() + " Date: " + booking.getSlot().getDate() + " Start Time: "
+				+ booking.getSlot().getTime() + " Length: " + length + " Staff: " + booking.getStaff().getName();
 
 		return bookingString;
 	}
@@ -26,17 +25,26 @@ public class BookingFactory {
 		bookingPane.setEditable(false);
 		bookingPane.setText(bookingString(booking));
 
-		// TODO Style text and pane
+		if (booking.getStatus().compareTo("Available") == 0) {
+			bookingPane.setBackground(Color.GREEN);
+		} else {
+			bookingPane.setBackground(Color.RED);
+		}
 
 		return bookingPane;
 	}
 
-	public static JButton bookingButton(Booking booking) {
-		JButton bookingButton = new JButton();
+	public static AvButton bookingButton(Booking booking) {
+		AvButton bookingButton = new AvButton(bookingString(booking));
+		bookingButton.setBooking(booking);
 
-		bookingButton.setText(bookingString(booking));
-
-		// TODO Style text and pane
+		if (booking.getStatus().compareTo("Available") == 0) {
+			bookingButton.setBackground(Color.GREEN);
+			bookingButton.setEnabled(true);
+		} else {
+			bookingButton.setBackground(Color.RED);
+			bookingButton.setEnabled(false);
+		}
 
 		return bookingButton;
 	}
@@ -45,10 +53,9 @@ public class BookingFactory {
 		JTextPane bookingPane = new JTextPane();
 
 		bookingPane.setEditable(false);
+		bookingPane.setBackground(Color.GREEN);
 		bookingPane
 				.setText("Type: Haircut" + "Date: 1/5" + "Start Time: 8:30 am" + "Length: 0.5 hrs" + "Staff: Daniel");
-
-		// Style text and pane
 
 		return bookingPane;
 	}
